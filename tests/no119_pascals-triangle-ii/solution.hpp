5f354ee224c0d6a432fdcb87edd629dc49079d40
OmegaWei1984 <omegawei1984@gmail.com>
2025-01-28 21:27:13 +0800

    fix: 119 ascals-triangle-ii

--- tests/no119_pascals-triangle-ii/solution.hpp
+++ tests/no119_pascals-triangle-ii/solution.hpp
@@ -20,7 +20,7 @@ public:
         vector<int> result(rowIndex + 1);
         result[0] = 1;
         for (int i = 1; i <= rowIndex; ++i) {
-            result[i] = result[i - 1] * (rowIndex - i + 1) / i;
+            result[i] = 1LL * result[i - 1] * (rowIndex - i + 1) / i;
         }
         return result;
     }
